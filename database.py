92dc57d721e1e71f0dc284f34e6d3c999592ba37
digitalpixel16 <analyticsdigitalpixel16@gmail.com>
2026-08-02 20:51:07 +0530

    Change database name from 'analytics_db' to 'postgres'

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ DB_USER = "postgres"
 DB_PASSWORD = quote_plus("!zWQT9jb%&P!d8B")
 DB_HOST = "db.qvbripbaosvdypxideyl.supabase.co"
 DB_PORT = "5432"
-DB_NAME = "analytics_db"
+DB_NAME = "postgres"
 
 engine = create_engine(
     f"postgresql://postgres:{DB_PASSWORD}@{DB_HOST}:5432/{DB_NAME}"
