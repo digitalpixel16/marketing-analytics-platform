2682d42e80a984c509817fe3aaab27c0386e218b
digitalpixel16 <analyticsdigitalpixel16@gmail.com>
2026-08-02 20:59:55 +0530

    Update database connection credentials
    updated the session pooler

--- database.py
+++ database.py
@@ -1,12 +1,9 @@
 from sqlalchemy import create_engine
 from urllib.parse import quote_plus
 
-DB_USER = "postgres"
+DB_USER = "postgres.qvbripbaosvdypxideyl"
 DB_PASSWORD = quote_plus("!zWQT9jb%&P!d8B")
-DB_HOST = "aws-1-ap-south-1.pooler.supabase.com"
-DB_PORT = "5432"
-DB_NAME = "postgres"
 
 engine = create_engine(
-    f"postgresql://postgres:{DB_PASSWORD}@{DB_HOST}:5432/{DB_NAME}"
+    f"postgresql://{DB_USER}:{DB_PASSWORD}@aws-1-ap-south-1.pooler.supabase.com:5432/postgres"
 )
